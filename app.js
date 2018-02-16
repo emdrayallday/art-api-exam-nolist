e2f03f2274d1599e053f52dfe49fc6b6db351975
Mark Johnson <markjohnson4542@gmail.com>
2018-02-16 13:38:20 -0500

    created script load/added bulkDocs to load-data

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
+const port = process.env.PORT || 4000
 app.get('/', function(req, res, next) {
   res.send('Welcome to the Art API. Manage all the paintings for much win.')
 })
+
+app.listn(port, () => console.log('MarkArt is running on port: ', port))
